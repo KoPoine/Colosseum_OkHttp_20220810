f561727b4d47ca6bedc77b7fcba9ac2b394dde6e
넵플러스 <kmc901206@gmail.com>
2022-08-10 15:40:48 +0900

    회원가입 API 작성 완료

--- app/src/main/java/com/neppplus/colosseum_okhttp_20220810/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_okhttp_20220810/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.colosseum_okhttp_20220810
 
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.colosseum_okhttp_20220810.databinding.ActivitySignUpBinding
 import com.neppplus.colosseum_okhttp_20220810.utils.ServerUtil
@@ -29,10 +30,56 @@ class SignUpActivity : BaseActivity() {
         binding.nickDupBtn.setOnClickListener {
             val inputNick = binding.nickEdt.text.toString()
 
-            checkDuplicate("Nick_NAME", inputNick, binding.emailDupTxt)
+            checkDuplicate("NICK_NAME", inputNick, binding.nickDupTxt)
         }
 
-        binding.signUpBtn.setOnClickListener {  }
+        binding.signUpBtn.setOnClickListener {
+
+//            [도전 과제] 번호로 기록된 회원가입 분기처리 코드 작성하기
+//             - 해당 번호의 검사를 통화하지 못한다면 => 회원 가입 처리 진행 X
+//              - 회원 가입 처리 진행 X => signUpBtn 뛰쳐나간다. return@setOnClickListener
+
+//            1. 이메일이 공백인가요?
+            val inputEmail = binding.emailEdt.text.toString()
+
+//            2. 비밀번호가 공백인가요?
+            val inputPw = binding.passwordEdt.text.toString()
+
+//            3. 닉네임이 공백인가?
+            val inputNick = binding.nickEdt.text.toString()
+
+//            4. 각 2가지 중복 검사를 모두 통과했나요?
+
+
+//            5. 실제 회원가입 처리 진행
+            ServerUtil.putRequestSignUp(
+                inputEmail,
+                inputPw,
+                inputNick,
+                object : ServerUtil.JsonResponseHandler{
+                    override fun onResponse(jsonObj: JSONObject) {
+                        val code = jsonObj.getInt("code")
+
+                        if (code == 200) {
+                            val dataObj = jsonObj.getJSONObject("data")
+                            val userObj = dataObj.getJSONObject("user")
+                            val nickname = userObj.getString("nick_name")
+                            runOnUiThread {
+                                Toast.makeText(mContext, "${nickname}님 가입을 환영합니다.", Toast.LENGTH_SHORT).show()
+                                finish()
+                            }
+                        }
+                        else {
+                            val message = jsonObj.getString("message")
+
+                            runOnUiThread {
+                                Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                })
+
+        }
     }
 
     override fun setValues() {
