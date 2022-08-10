95e227144268a8c594cceb7b721b80ff8c7691f7
넵플러스 <kmc901206@gmail.com>
2022-08-10 10:11:23 +0900

    로그인 로직 구현(임시)

--- app/src/main/java/com/neppplus/colosseum_okhttp_20220810/LoginActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_okhttp_20220810/LoginActivity.kt
@@ -2,8 +2,12 @@ package com.neppplus.colosseum_okhttp_20220810
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.colosseum_okhttp_20220810.databinding.ActivityLoginBinding
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class LoginActivity : BaseActivity() {
 
@@ -22,7 +26,42 @@ class LoginActivity : BaseActivity() {
             val inputEmail = binding.emailEdt.text.toString()
             val inputPw = binding.passwordEdt.text.toString()
 
-//            서버에 Request를 보내는 로직
+//            서버에 Request를 보내는 로직 (예시)
+            val url = "http://54.180.52.26/user"
+
+            val formData = FormBody.Builder()
+                .add("email", inputEmail)
+                .add("password", inputPw)
+                .build()
+
+            val request = Request.Builder()
+                .url(url)
+                .post(formData)
+                .build()
+
+            val client = OkHttpClient()
+
+            client.newCall(request).enqueue(object : Callback{
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val bodyString = response.body!!.string()
+                    val jsonObj = JSONObject(bodyString)
+                    val code = jsonObj.getInt("code")
+
+                    if (code == 200) {
+//                        로그인 성공
+                    }
+                    else {
+                        runOnUiThread {
+                            Toast.makeText(mContext, "로그인에 실패", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            })
+
         }
     }
 
