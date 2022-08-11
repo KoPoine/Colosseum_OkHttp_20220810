11ba1bb735803887f7ff6baf1398427a2ee34efc
넵플러스 <kmc901206@gmail.com>
2022-08-11 10:05:26 +0900

    RecyclerView 어댑터 뼈대 생성

---
+++ app/src/main/java/com/neppplus/colosseum_okhttp_20220810/adapters/TopicRecyclerAdapter.kt
@@ -0,0 +1,34 @@
+package com.neppplus.colosseum_okhttp_20220810.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.colosseum_okhttp_20220810.R
+import com.neppplus.colosseum_okhttp_20220810.datas.TopicData
+
+class TopicRecyclerAdapter(
+    val mContext : Context, val mList : List<TopicData>
+) : RecyclerView.Adapter<TopicRecyclerAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder (view : View) : RecyclerView.ViewHolder(view) {
+        fun bind(item : TopicData){
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val row = LayoutInflater.from(mContext).inflate(R.layout.topic_list_item, parent, false)
+        return MyViewHolder(row)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(mList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+}
