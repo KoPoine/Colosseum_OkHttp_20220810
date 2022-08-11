1f7052a166a5e24c66c818ed91df82cdcd13c28c
넵플러스 <kmc901206@gmail.com>
2022-08-11 10:00:55 +0900

    데이터 표현용 클래스 제작

---
+++ app/src/main/java/com/neppplus/colosseum_okhttp_20220810/datas/TopicData.kt
@@ -0,0 +1,14 @@
+package com.neppplus.colosseum_okhttp_20220810.datas
+
+class TopicData() {
+
+//    생성자 : 기본 생성자 유지
+//    멤버변수만 따로 추가 -> JSON 파싱해서 변수에 채워넣자
+//    멤버변수 : 서버의 데이터를 보고 -> 담아두는 용도의 변수들로 만들자.
+
+    var id = 0  // Int가 들어올 자리라는 표시
+    var title = ""  // String이 들어올 자리
+    var imageUrl = ""
+    var replyCount = 0
+
+}
