316b5e96f62090c38160a589429769969e43e2e3
넵플러스 <kmc901206@gmail.com>
2022-08-11 11:04:23 +0900

    MainActivity에서 실행되던 setCustomActionBar 함수를 BaseActivity 이동 => 모든 Acitivity에서 실행 되도록 설정

--- app/src/main/java/com/neppplus/colosseum_okhttp_20220810/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_okhttp_20220810/MainActivity.kt
@@ -46,8 +46,6 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-        setCustomActionBar()
-
         getTopicListFromServer()
 
         mTopicAdapter = TopicRecyclerAdapter(mContext, mTopicList)
@@ -93,27 +91,6 @@ class MainActivity : BaseActivity() {
                 }
             }
         })
-    }
-
-    fun setCustomActionBar () {
-//        기존 액션바를 담아줄 변수
-        val defaultActionBar = supportActionBar!!
-
-//        기존 액션바를 커스텀 모드로 변경 > 우리가 만든 xml로 적용
-//        ActionBar는 Androidx에서 주는 걸로 자동 완성
-        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
-        defaultActionBar.setCustomView(R.layout.custom_action_bar)
-
-//        Toolbar를 찾아내서 > 양 옆의 여백 제거 -> 모든 영역이 커스텀 뷰
-//        Toolbar 찾는다 > 우리의 액션바의 부모 > Androidx에서 제공하는 Toolbar 형 변환
-        val myToolbar = defaultActionBar.customView.parent as Toolbar
-        myToolbar.setContentInsetsAbsolute(0,0)
-
-        val backIcon = defaultActionBar.customView.findViewById<ImageView>(R.id.backIcon)
-        val titleTxt = defaultActionBar.customView.findViewById<TextView>(R.id.titleTxt)
-        val profileIcon = defaultActionBar.customView.findViewById<ImageView>(R.id.profileIcon)
-
-        profileIcon.visibility = View.VISIBLE
     }
 
 }
