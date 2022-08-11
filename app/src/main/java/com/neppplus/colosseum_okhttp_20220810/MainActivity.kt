ded7a78c1b4a3a0527aadd61bece9e27acf8858a
넵플러스 <kmc901206@gmail.com>
2022-08-11 09:49:59 +0900

    가져온 TopicArr에서 하나의 TopicObject를 추출

--- app/src/main/java/com/neppplus/colosseum_okhttp_20220810/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_okhttp_20220810/MainActivity.kt
@@ -3,9 +3,12 @@ package com.neppplus.colosseum_okhttp_20220810
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.neppplus.colosseum_okhttp_20220810.databinding.ActivityMainBinding
 import com.neppplus.colosseum_okhttp_20220810.utils.ContextUtil
+import com.neppplus.colosseum_okhttp_20220810.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -32,7 +35,28 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        getTopicListFromServer()
+    }
 
+    fun getTopicListFromServer() {
+        val token = ContextUtil.getLoginToken(mContext)
+        ServerUtil.getRequestMainInfo(token, object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(jsonObj: JSONObject) {
+                val dataObj = jsonObj.getJSONObject("data")
+                val topicsArr = dataObj.getJSONArray("topics")
+
+//                Log.d("topics", topicsArr.toString())
+
+//                [  ] => {  }, {  }, ... 순서에 맞춰서 설정된 JsonObj를 뽑아오자(배열에서)
+                for (i in 0 until topicsArr.length()) {
+//                    JSON 파싱의 {  } => JSONArray 안에서 JSONObject를 하나씩 get함수 사용
+                    val topicObj = topicsArr.getJSONObject(i)
+
+                    Log.d("받아낸 주제", topicObj.toString())
+
+                }
+            }
+        })
     }
 
 }
